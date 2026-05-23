95a753d95f11e81d6842de23804e2e994a41f2cc
maulanaakbar1 <hasbymakbar1@gmail.com>
2026-05-24 00:55:22 +0700

    gnti warna icon

--- resources/views/admin/ekstrakurikuler.blade.php
+++ resources/views/admin/ekstrakurikuler.blade.php
@@ -68,8 +68,27 @@
                 </div>
                 <div class="flex gap-1">
                     {{-- Tombol Edit --}}
-                    <button @click="editData = {id: '{{ $e->id }}',nama: '{{ $e->nama }}',nama_satuan: '{{ $e->nama_satuan }}',deskripsi: '{{ $e->deskripsi }}'}; openEdit = true">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
+                    <button 
+                        @click="editData = {
+                            id: '{{ $e->id }}',
+                            nama: '{{ $e->nama }}',
+                            nama_satuan: '{{ $e->nama_satuan }}',
+                            deskripsi: '{{ $e->deskripsi }}'
+                        }; openEdit = true"
+                        class="p-2 text-amber-500 hover:bg-amber-50 rounded-lg transition"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                            class="h-5 w-5" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            stroke="currentColor">
+                            
+                            <path stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                stroke-width="2" 
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                        
+                        </svg>
                     </button>
                     
                     {{-- Tombol Hapus dengan SweetAlert2 Konfirmasi --}}
