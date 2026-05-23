607857305e49aca7c33ca3378bdc3c1fe9364703
maulanaakbar1 <hasbymakbar1@gmail.com>
2026-05-23 23:51:22 +0700

    .

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside :class="sidebarOpen ? 'translate-x-0' : '-translate-x-full'" 
-       class="fixed inset-y-0 left-0 z-50 w-72 bg-white border-r border-slate-200 flex flex-col transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:flex">
+        class="fixed inset-y-0 left-0 z-50 w-72 bg-white border-r border-slate-200 flex flex-col transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:flex">
     
     <div class="p-6 flex items-center justify-between">
 
@@ -8,13 +8,13 @@
 
             <div class="h-12 w-12 rounded-xl overflow-hidden shadow-lg shadow-blue-200 bg-white border border-slate-100">
                 <img 
-                    src="{{ asset('images/braga1.jpeg') }}" 
+                    src="{{ asset('images/logo_smk.jpg') }}" 
                     alt="Logo Sekolah"
                     class="h-full w-full object-cover">
             </div>
 
             <div>
-            <h1 class="text-2xl font-bold text-red-800">BRAGA</h1>
+            <h1 class="text-2xl font-bold text-blue-800">Absensi</h1>
             <p class="text-[10px] text-slate-400 font-bold uppercase tracking-widest mt-1">
                 {{ Auth::user()->role }} Panel
             </p>
@@ -30,7 +30,7 @@
     
     <nav class="flex-1 px-4 space-y-2">
         <a href="{{ route('admin.dashboard') }}" 
-           class="flex items-center gap-3 {{ Request::is('admin/dashboard') ? 'bg-blue-50 text-blue-600' : 'text-slate-500 hover:bg-slate-50' }} px-4 py-3 rounded-xl font-semibold transition">
+            class="flex items-center gap-3 {{ Request::is('admin/dashboard') ? 'bg-blue-50 text-blue-600' : 'text-slate-500 hover:bg-slate-50' }} px-4 py-3 rounded-xl font-semibold transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
             </svg>
